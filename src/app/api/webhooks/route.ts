bc27b8bbf64c6a6177e688b714b2a589f19178ce
Tarun <tarun.mahajan@elxoinc.com>
2024-08-06 16:26:35 +0530

    new 6.1

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -4,7 +4,6 @@ import { headers } from 'next/headers'
 import { NextResponse } from 'next/server'
 import Stripe from 'stripe'
 // import { Resend } from 'resend'
-// import OrderReceivedEmail from '@/components/emails/OrderReceivedEmail'
 
 // const resend = new Resend(process.env.RESEND_API_KEY)
 
@@ -70,25 +69,6 @@ export async function POST(req: Request) {
           },
         },
       })
-
-    //   await resend.emails.send({
-    //     from: 'CaseCobra <hello@joshtriedcoding.com>',
-    //     to: [event.data.object.customer_details.email],
-    //     subject: 'Thanks for your order!',
-    //     react: OrderReceivedEmail({
-    //       orderId,
-    //       orderDate: updatedOrder.createdAt.toLocaleDateString(),
-    //       // @ts-ignore
-    //       shippingAddress: {
-    //         name: session.customer_details!.name!,
-    //         city: shippingAddress!.city!,
-    //         country: shippingAddress!.country!,
-    //         postalCode: shippingAddress!.postal_code!,
-    //         street: shippingAddress!.line1!,
-    //         state: shippingAddress!.state,
-    //       },
-    //     }),
-    //   })
     }
 
     return NextResponse.json({ result: event, ok: true })
