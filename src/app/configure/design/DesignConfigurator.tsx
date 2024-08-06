33f4597744416660d19a1c60e2ce491668c920b8
Tarun <tarun.mahajan@elxoinc.com>
2024-08-06 16:07:41 +0530

    new 6

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -381,9 +381,9 @@ const DesignConfigurator = ({
                 )}
               </p>
               <Button
-                // isLoading={isPending}
+                isLoading={isPending}
                 disabled={isPending}
-                // loadingText="Saving"
+                loadingText="Saving"
                 onClick={() =>
                   saveConfig({
                     configId,
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -72,6 +72,10 @@ const config = {
             transform:"translateY(-50%)"
           }
         },
+        flashing: {
+          '0%, 100%': { opacity: '0.2' },
+          '20%': { opacity: "1" },
+        },
         "fade-in":{
           from:{
             opacity:'0'
@@ -86,6 +90,7 @@ const config = {
         "fade-in":"fade-in 0.5s linear forwards",
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        flashing: 'flashing 1.4s infinite linear',
       },
     },
   },
